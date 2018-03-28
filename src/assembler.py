9443345d4b7ac56170a810b6b89b86f1ae444734
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-28 19:44:00 +0200

    created basic files

--- src/assembler.py
+++ src/assembler.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3
 
 import tensorflow as tf
-
-print("hello world")
---
+++ src/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import splitter, classifier, assembler
+
+import tensorflow as tf
+import sys
+import os
+
+def main():
+	# TODO
+
+if __name__ == "__main__":
+	main()
