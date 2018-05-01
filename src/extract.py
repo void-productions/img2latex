39639e486ce1d193dcf3633883e4463a855c2ae9
Bluemi <bruno.schilling@protonmail.ch>
2018-05-01 16:37:15 +0200

    extract normalizes train images now

--- src/extract.py
+++ src/extract.py
@@ -13,27 +13,49 @@ LABEL_FILE = "labels.yaml"
 IMAGE_ENDING = ".png"
 
 
-def image_files_to_array(image_files, number_of_images):
+def rectify_image(image):
+	"""
+	Adds padding to rectify the image
+	"""
+
+	height = image.shape[0]
+	width = image.shape[1]
+	depth = image.shape[2]
+
+	maxi = max(width, height)
+
+	rectified_image = np.ones((maxi, maxi, depth))
+
+	rectified_image[:height,:width] = image
+
+	return rectified_image
+
+
+
+def image_files_to_array(image_files, number_of_images, image_shape):
 	"""
 	Reads the given image files and returns one numpy array filled with all images.
 	"""
 
-	width = -1
-	height = -1
-	depth = 1
-	initialized = False
+	height = image_shape[0]
+	width = image_shape[1]
+	depth = image_shape[2]
+
+	if depth == 3:
+		color_mode = 1 # colored image
+	elif depth == 1:
+		color_mode = 0 # grayscale image
+	else:
+		raise ValueError("image_shape[2] (depth) has an invalid value: {}".format(depth))
+
+	images = np.empty((number_of_images, height * width * depth))
 
 	for index, path in enumerate(image_files):
 		if not os.path.isfile(path):
 			raise IOError("could not open '{}'".format(path))
-		img = cv2.imread(path)
-		if not initialized:
-			height = img.shape[0]
-			width = img.shape[1]
-			if len(img.shape) == 3:
-				depth = img.shape[2]
-			initialized = True
-			images = np.empty((number_of_images, height * width * depth))
+		img = cv2.imread(path, color_mode)
+		img = rectify_image(img)
+		img = cv2.resize(img, dsize=image_shape[:2])
 		images[index] = np.reshape(img, height * width * depth)
 
 	return images
@@ -64,13 +86,15 @@ def extract_image_dataset(path):
 	"""
 	pass
 
-def extract_yaml_dataset(path):
+def extract_yaml_dataset(path, image_shape):
 	"""
 	Reads the yaml-dataset with the specified path and returns a tuple(x_data, y_data)
 	A yaml-dataset directory should contain the directories images/ and labels/
 
 	:param path: Path to the yaml-dataset directory
 	:type path: str
+	:param image_shape: The forced image shape of the x_data
+	:type image_shape: tuple(height, width)
 	"""
 
 	image_directory = os.path.join(path, IMAGE_DIRECTORY)
@@ -96,7 +120,7 @@ def extract_yaml_dataset(path):
 	image_files = map(lambda x: os.path.join(image_directory, x) + IMAGE_ENDING,
 			description.keys())
 
-	x_data = image_files_to_array(image_files, number_of_images)
+	x_data = image_files_to_array(image_files, number_of_images, image_shape)
 
 	return (x_data, labels)
 
